546095e617925e70cd2187c543e277b0bd0a72eb
Tanvi Kapurwan <125541005+TanviKapurwan23@users.noreply.github.com>
2025-01-08 17:27:56 +0530

    Create queueSize.cpp

---
+++ Queue/queueSize.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    std::queue<int> q;
+
+    // Adding elements to the queue
+    q.push(10);
+    q.push(20);
+    q.push(30);
+
+    
+    cout<<q.size();
+    return 0;
+}
